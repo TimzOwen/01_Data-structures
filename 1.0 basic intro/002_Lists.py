91d1bcf11d4624ab8199dbb43abeb90597bebdb2
Timz Owen <ktimothy@kabarak.ac.ke>
2021-01-09 17:37:14 +0300

    Update 002_Lists.py
    updated on lists

--- 1.0 basic intro/002_Lists.py
+++ 1.0 basic intro/002_Lists.py
@@ -1,1 +1,30 @@
 
+# Lists
+
+# Createing a list
+
+list1 = [10,20,30,40,50,60,70]
+list2 = ["server","SQL","RJ-45","Technology"]
+
+print(list1)
+print(list2)
+
+# # Accessing elements:
+
+print("Element at index 0 : ", list2[0])
+print("Element from index 0-4 is :", list1[0:5])
+
+#  updating and element
+
+list1[0] = 300
+
+# deleting elments:
+
+del list1[1]
+
+# print output
+
+print(list1)
+
+
+
