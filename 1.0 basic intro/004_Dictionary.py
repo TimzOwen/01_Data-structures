c20f78fe20872820558882de3209411e6e09cdb4
Timz Owen <ktimothy@kabarak.ac.ke>
2021-01-11 14:22:10 +0300

    Update 004_Dictionary.py
    working with dictionary

--- 1.0 basic intro/004_Dictionary.py
+++ 1.0 basic intro/004_Dictionary.py
@@ -1,1 +1,30 @@
 
+# Dictionary
+
+#create a new dictionary
+
+dict1 = {"Timz":23,"Age":67,"Country":"Kenya"}
+
+print(dict1)
+
+# Acess elements:
+print("Timz's age is : ", dict1["Age"])
+print("From : ",dict1["Country"])
+
+# Modiying elements of a dictionary
+
+dict1['Age'] = 21  # updating existing data
+
+dict1['Company'] = "Central Bank of Kenya"  # adding a new entry
+
+# deleting:
+
+dict1.clear() # removes all elements in a dictionary
+
+del dict1['Name'] # Removes elements
+
+del dict1  # Deletes an entire dictionary
+
+
+
+
